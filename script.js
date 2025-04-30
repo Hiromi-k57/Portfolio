4f0b02fdc691a4366123b784436b8ac0da81c428
Hiromi <nim57oom@gmail.com>
2025-04-30 14:46:07 +0200

    portfolio 1

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+
+document.querySelectorAll('.flip_card').forEach(card=>{
+    card.addEventListener('click', function() {
+        this.classList.toggle('is-flipped');
+    });
+})
+;
+const textFromLeft = document.querySelectorAll(".textFromLeft");
+const ovserverFromLeft = new IntersectionObserver(fromLeft,{rootMargin: "-30%"});
+
+textFromLeft.forEach(text =>{
+    ovserverFromLeft.observe(text)
+}) 
+/**
+ * 
+ * @param {IntersectionObserverEntry[]} entres 
+ */
+function fromLeft (entres)
+{
+    console.log(entres);
+    
+    entres.forEach((entree)=>{
+        
+        if(entree.isIntersecting){
+            entree.target.classList.add("show")
+        }
+        else if(entree.rootBounds.top < entree.boundingClientRect.top){
+            entree.target.classList.remove("show")
+        }
+    })
+}
